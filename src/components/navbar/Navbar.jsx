90d90cde666a75e1a5a1baf72bf0e549ff7ad086
VeniVersion <82077694+VeniVersion@users.noreply.github.com>
2023-01-20 21:32:20 +0000

    Pending changes exported from your codespace

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,37 +7,37 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
+    <div className="sv__navbar">
+      <div className="sv__navbar-links">
+        <div className="sv__navbar-links_logo">
           <img src={logo} />
         </div>
-        <div className="gpt3__navbar-links_container">
+        <div className="sv__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#sv">Sports Vision</a></p>
+          <p><a href="#possibility">Vizworld</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
-      <div className="gpt3__navbar-sign">
+      <div className="sv__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
-      <div className="gpt3__navbar-menu">
+      <div className="sv__navbar-menu">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
-          <div className="gpt3__navbar-menu_container-links">
+        <div className="sv__navbar-menu_container scale-up-center">
+          <div className="sv__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#sv">Sports Vision</a></p>
+            <p><a href="#possibility">Vizworld</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
           </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
+          <div className="sv__navbar-menu_container-links-sign">
             <p>Sign in</p>
             <button type="button">Sign up</button>
           </div>
--- src/containers/spvi/spvi.jsx
+++ src/containers/spvi/spvi.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Feature from '../../components/feature/Feature';
-import './whatGPT3.css';
+import './spvi.css';
 
 const WhatGPT3 = () => (
-  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
-    <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+  <div className="sv__spvi section__margin" id="spvi">
+    <div className="sv__spvi-feature">
+      <Feature title="sports vision" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
     </div>
-    <div className="gpt3__whatgpt3-heading">
+    <div className="sv__spvi-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the Library</p>
     </div>
-    <div className="gpt3__whatgpt3-container">
+    <div className="sv__spvi-container">
       <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
       <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
       <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
@@ -19,4 +19,4 @@ const WhatGPT3 = () => (
   </div>
 );
 
-export default WhatGPT3;
+export default spvi;
